dbfb0aff0d07a24b22255e4a860bfcc587cd11ec
efearob_platform <you@example.com>
2023-11-30 15:22:16 +0300

    Toka Fix

--- [synergy]/efearob-base/fundamental/client.lua
+++ [synergy]/efearob-base/fundamental/client.lua
@@ -157,32 +157,63 @@ end, false)
 --    TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(vehicle))
 -- end)
 
+
+local skinData = {
+    ["hair"] = {
+        item = 0,
+        texture = 0,
+        defaultItem = 0,
+        defaultTexture = 0,
+    },
+    ["face2"] = {
+        item = 0,
+        texture = 0,
+        defaultItem = 0,
+        defaultTexture = 0,
+    },
+    ["facemix"] = {
+        skinMix = 0,
+        shapeMix = 0,
+        defaultSkinMix = 0.0,
+        defaultShapeMix = 0.0,
+    },
+}
+
+local defaultHairMale = 186
+local defaultHairFemale = 192
+local oldHair = { item = -1, texture = -1 }
+local sinirS = 0
 RegisterCommand("toka", function()
-    local playerPed = PlayerPedId()
-    local pedAppearance = exports['illenium-appearance']:getPedAppearance(playerPed)
-
-    if not oldHair.active then
-        oldHair.texture = pedAppearance["hair"].color
-        oldHair.item = pedAppearance["hair"].style
-    end
-
-    local defaultHair = defaultHairFemale
-    if GetEntityModel(playerPed) == 1885233650 then
-        defaultHair = defaultHairMale
-    end -- Erkek Model
-
-    if not oldHair.active then
-
-        SetPedComponentVariation(playerPed, 2, defaultHair, 0, 0)
-        oldHair.active = true
-    else
-
-        SetPedComponentVariation(playerPed, 2, oldHair.item, 0, 0)
-        oldHair.active = false
-    end
-
-    SetPedHairColor(playerPed, oldHair.texture)
-
+   if skinData["hair"].item == 0 then
+       skinData["hair"].item = GetPedDrawableVariation(PlayerPedId(), 2)
+   end
+   if GetGameTimer() > sinirS then
+       sinirS = GetGameTimer() + 5000
+       local playerPed = PlayerPedId()
+       oldHair.texture = GetPedHairColor(playerPed) 
+       oldHair.texture2 = GetPedHairHighlightColor(playerPed)
+       local defaultHair = defaultHairFemale
+       if GetEntityModel(playerPed) == 1885233650 then 
+           defaultHair = defaultHairMale 
+       end -- Erkek Model
+       if oldHair.item == -1 then 
+           oldHair.item = defaultHair 
+       end
+       if oldHair.item == defaultHair then
+           ExecuteCommand('me Tokasını çıkartıp saçını bağlamaya başlar')
+           Citizen.Wait(1000)
+           SetPedComponentVariation(playerPed, 2, defaultHair, 0, 0)
+           oldHair.item = skinData["hair"].item
+           Citizen.Wait(1000)
+           ExecuteCommand('do Saçında toka olduğu görülebilir')
+       else
+           ExecuteCommand('me Saçındaki tokayı çıkartır')
+           Citizen.Wait(1000)
+           SetPedComponentVariation(playerPed, 2, oldHair.item, 0, 0)
+           oldHair.item = defaultHair
+       end
+       SetPedHairColor(playerPed, oldHair.texture, oldHair.texture2)
+   end
 end)
 
 
